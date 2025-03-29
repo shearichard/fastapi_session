b0ee162b13230feaeaa7603920950fd704ab4bcd
Richard Shea <rshea@thecubagroup.com>
2025-03-29 22:14:25 +1300

    Tidy up and put the sessions under stronger control.
    Session cookies don't live as long.
    
    Also add a date/time to the session data save for diagnostic purposes

--- src/main.py
+++ src/main.py
@@ -1,31 +1,45 @@
+from datetime import datetime, timezone
+#
 from fastapi import FastAPI, Depends, HTTPException, Request, status, Header, Cookie
 from fastapi.responses import RedirectResponse, HTMLResponse
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 #
-from .utils import get_env_variable
+from .utils import get_env_variable, get_local_dt_iso_with_tz_info
 
 
 # Create the FastAPI app
 app = FastAPI()
 # Add session middleware
 SESSION_MIDDLEWARE_KEY= get_env_variable("SESSION_MIDDLEWARE_KEY")
-app.add_middleware(SessionMiddleware, secret_key=SESSION_MIDDLEWARE_KEY)
-
-# Example protected endpoint
-@app.get("/hello")
-async def hello():
-    return {"message": "Welcome to the session prototype"}
-
+app.add_middleware( SessionMiddleware, 
+                    secret_key=SESSION_MIDDLEWARE_KEY,
+                    session_cookie="session_prototype",
+                    max_age=None,
+                    same_site='strict')
+#
+#
 
 @app.get("/set/")
 async def set_session(request: Request):
+
+    local_dt_iso_with_tz_info=get_local_dt_iso_with_tz_info()
+    #
     request.session['favourite_colour'] = 'green'
+    request.session['favourite_colour_last_changed'] = local_dt_iso_with_tz_info
+    #
     return {"message": "Session set!"}
 
 
 @app.get("/get/")
 async def get_session(request: Request):
-    value = request.session.get('favourite_colour', 'no favourite colour set')
-    return {"favourite_colour": value}
+    colour = request.session.get('favourite_colour', 'no favourite colour set')
+    colour_last_change  = request.session.get('favourite_colour_last_changed', 'no date of previous update for favourite colour')
+    return {"favourite_colour": colour, "last_update_time": colour_last_change}
+
+
+# Placeholder endpoint
+@app.get("/")
+async def hello():
+    return {"message": "Welcome to the session prototype"}
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 A bunch of utility functions for use across the project
 '''
 import os
+from datetime import datetime, timezone
 
 
 def get_env_variable(var_name):
@@ -12,3 +13,14 @@ def get_env_variable(var_name):
         #This will only work in Python >= 3.11
         #e.add_note("Set the %s environment variable" % var_name)
         raise
+
+
+def get_local_dt_iso_with_tz_info():
+    # get current datetime in UTC
+    dt = datetime.now(timezone.utc)
+    # add local timezone information to datetime
+    tz_dt = dt.astimezone()
+    # Get current iso 8601 format datetime string including the default timezone
+    iso_date = tz_dt.isoformat()
+    #
+    return(iso_date)
